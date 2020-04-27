2159eb82697079ee02c0fca053696a5740905b4a
Ratchaneekorn Suwanwong <r.suwanwong@gmail.com>
2020-04-27 21:20:40 +0700

    api

--- src/pages/api/covid19.js
+++ src/pages/api/covid19.js
@@ -3,33 +3,36 @@ export default function covid19(req, res) {
   const axios = require('axios')
   const csv = require('csv-parser')
 
-  const url =
-    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-  const apiPath = 'api/covid19.js'
+  async function convertData() {
+    const url =
+      'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    const apiPath = 'api/covid19.js'
+    const writer = fs.createWriteStream(apiPath)
 
-  axios({
-    method: 'get',
-    url: url,
-    responseType: 'stream',
-  }).then(function(response) {
-    response.data.pipe(fs.createWriteStream(apiPath))
-  })
+    const getData = await axios({
+      method: 'get',
+      url: url,
+      responseType: 'stream',
+    }).then(function(response) {
+      response.data.pipe(writer)
+    })
 
-  // let results = []
-  // fs.createReadStream(apiPath)
-  //   .pipe(
-  //     csv({
-  //       headers: false,
-  //     }),
-  //   )
-  //   .on('data', data => results.push(data))
-  //   .on('end', () => {
-  //     console.log('results => ', results)
-  //     // return results
-  //   })
+    let results = []
+    fs.createReadStream(apiPath)
+      .pipe(
+        csv({
+          headers: false,
+        }),
+      )
+      .on('data', data => results.push(data))
+      .on('end', () => {
+        // console.log('results => ', results)
 
-  let response = { name: 'John Doe' }
-  let status = 200
+        // let response = { name: 'John Doe' }
+        // let status = 200
 
-  res.status(status).json(response)
+        res.status(200).json(results)
+      })
+  }
+  convertData()
 }
