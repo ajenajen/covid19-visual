3822f6289aad3125c2c5c3a9f6a8d35769db8042
Ratchaneekorn Suwanwong <r.suwanwong@gmail.com>
2020-05-01 00:55:46 +0700

    fixed api

--- src/pages/api/covid19.js
+++ src/pages/api/covid19.js
@@ -1,23 +1,64 @@
-export default function covid19(req, res) {
-  const request = require('request')
-  const csv = require('csv-stream')
-  const csvStream = csv.createStream()
+const axios = require('axios')
+const parse = require('csv-parse/lib/sync')
 
-  const url =
-    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+const urlConfirmed =
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+const urlRecovered =
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
+const urlDeaths =
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 
-  let results = []
-  request(url)
-    .pipe(csvStream)
-    .on('error', function(err) {
-      console.error(err)
+async function extract(urlpath) {
+  const getData = await axios.get(urlpath)
+  const [headers, ...rows] = await parse(getData.data) // แยก key : [rows] กับ value : rows
+  const [province, country, lat, long, ...dates] = headers // เอา key มาแยก ,เปลี่ยน key name 4 ตัวแรก ที่เหลือต่อด้วย dates
+  const countList = {}
+
+  // Fixed date formats เปลี่ยน / เป็น -
+  const normalDates = dates.map(date => {
+    const [month, day] = date.split('/')
+    return `2020-${month}-${day}`
+  })
+
+  rows.forEach(([province, country, lat, long, ...dates]) => {
+    // console.log('country', country) // ได้ forEach country ทั้งหมด , เอามาใช้แค่ country แต่แตกออกมาทั้งหมดเพื่อเอาตำแหน่ง
+    // console.log('value set', [province, country, lat, long, ...dates]) // ได้ country ซ้ำมาด้วย
+
+    countList[country] = countList[country] || {} // ทุกๆการ วนทีละ country  จับยัดใส่ เป็น key ของ array countList
+    // country ไม่ซ้ำ เพราะที่ซ้ำจะโดนรวม
+    // console.log('countList', countList) // ได้ countList ที่มี key เป็น ประเทศทั้งหมด
+
+    normalDates.forEach((date, val) => {
+      // loop array normalDates แยก key, value ของ date นั้นๆ
+      countList[country][date] = countList[country][date] || 0 // สร้าง key[date] ใน countList[ประเทศนั้นๆ]
+      countList[country][date] += +dates[val] // จับยัดใส่ array countList[ประเทศนั้นๆ] ทุกเมืองของประเทศนั้นๆ
     })
-    .on('data', row => {
-      results.push(row)
-    })
-    .on('end', () => {
-      res.status(200).json(JSON.stringify(results, null, 2))
-    })
+  })
+
+  // console.log('countList', countList)
+  return [countList, normalDates]
 }
 
-// how to cache , stream for one time a day ?
+export default async function covid19(req, res) {
+  const [confirmed, dates] = await extract(urlConfirmed)
+  const [recovered] = await extract(urlRecovered)
+  const [deaths] = await extract(urlDeaths)
+
+  const countries = Object.keys(confirmed) // ex: [confirmed['Thailand']]
+  const results = {}
+
+  countries.forEach(country => {
+    // console.log(country)
+    results[country] = dates.map(date => {
+      // console.log(date)
+      return {
+        date,
+        confirmed: confirmed[country][date],
+        deaths: deaths[country][date],
+        recovered: recovered[country][date],
+      }
+    })
+  })
+
+  res.status(200).json(JSON.stringify(results, null, 2))
+}
--- src/pages/api/miniquiz.js
+++ src/pages/api/miniquiz.js
@@ -1,35 +1,29 @@
 const confirmed = [
-  {
-    country: 'America',
-    value: 1000,
-  },
-  {
-    country: 'China',
-    value: 100,
-  },
+  { country: 'America', value: 1000 },
+  { country: 'China', value: 100 },
 ]
-
 const recovered = [
-  {
-    country: 'America',
-    value: 300,
-  },
-  {
-    country: 'China',
-    value: 30,
-  },
+  { country: 'America', value: 300 },
+  { country: 'China', value: 30 },
 ]
-
 const deaths = [
-  {
-    country: 'America',
-    value: 100,
-  },
-  {
-    country: 'China',
-    value: 10,
-  },
+  { country: 'America', value: 100 },
+  { country: 'China', value: 10 },
 ]
+/*
+const output = {
+  'America': {
+    confirmed: 1000,
+    recovered: 300,
+    deaths: 100
+  },
+  'China': {
+    confirmed: 100,
+    recovered: 30,
+    deaths: 10
+  }
+}
+*/
 
 const confirmedCase = confirmed.map(row => {
   return { country: row.country, confirmed: row.value }
@@ -40,10 +34,6 @@ const recoveredCase = recovered.map(row => {
 const deathsCase = deaths.map(row => {
   return { country: row.country, deaths: row.value }
 })
-
-// const output = confirmedCase.map((item, i) =>
-//   Object.assign({}, item, recoveredCase[i], deathsCase[i]),
-// )
 
 const results = confirmedCase.map(item => ({
   ...item,
@@ -61,24 +51,49 @@ results.map(row => {
     },
   })
 })
-
 // console.log(output)
 
-/*
-const output = {
-  'America': {
-    confirmed: 1000,
-    recovered: 300,
-    deaths: 100
-  },
-  'China': {
-    confirmed: 100,
-    recovered: 30,
-    deaths: 10
+// Method 1: Straight Forward
+const method1 = confirmed.reduce((prev, cur) => {
+  return {
+    ...prev,
+    [cur.country]: {
+      confirmed: confirmed.find(item => item.country === cur.country)?.value,
+      recovered: recovered.find(item => item.country === cur.country)?.value,
+      deaths: deaths.find(item => item.country === cur.country)?.value,
+    },
   }
-}
-*/
+}, {})
+
+// Method 2: Prepare data before reduce
+const rawData = { confirmed, recovered, deaths }
+// console.log('rawData', rawData)
+
+const preparedData = Object.keys(rawData).map(key => {
+  return rawData[key].reduce((prev, cur) => {
+    return {
+      ...prev,
+      [cur.country]: cur.value,
+    }
+  }, {})
+})
+
+const method2 = Object.keys(preparedData[0]).reduce((prev, cur) => {
+  const [preparedConfirmed, preparedRecovered, preparedDeaths] = preparedData
+  // console.log('preparedData', cur)
+  return {
+    ...prev,
+    [cur]: {
+      confirmed: preparedConfirmed[cur],
+      recovered: preparedRecovered[cur],
+      deaths: preparedDeaths[cur],
+    },
+  }
+}, {})
+
+console.log('method1', method1)
+console.log('method2', method2)
 
 export default function miniquiz(req, res) {
-  res.status(200).json(JSON.stringify(output, null, 2))
+  res.status(200).json(JSON.stringify(method2, null, 2))
 }
