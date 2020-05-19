0caa9c6703be96753641c60eca0a038c594b83f7
Ratchaneekorn Suwanwong <r.suwanwong@gmail.com>
2020-05-20 00:57:10 +0700

    fetch CardStatus dailyUpdate

--- src/components/home/DailyUpdate.js
+++ src/components/home/DailyUpdate.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import { Flex, Box } from '@grid'
-import MyCountryUpdate from './MyCountryUpdate'
+import CardStatus from './CardStatus'
+
+import * as CaseService from '@features/case/services'
+import { Fetch } from '@lib/api'
 
 export default function DailyUpdate() {
   return (
-    <Flex flexWrap="wrap" justifyContent="flex-end" mx={'-7px'}>
-      <Box width={[1 / 2, 1, 1, 3.25 / 10]} px={'7px'} mb={'15px'}>
-        <MyCountryUpdate type={'confirmed'}></MyCountryUpdate>
-      </Box>
-      <Box width={[1 / 2, 1 / 3, 3 / 9, 2.25 / 10]} px={'7px'} mb={'10px'}>
-        <MyCountryUpdate type={'recovered'}></MyCountryUpdate>
-      </Box>
-      <Box width={[1 / 2, 1 / 3, 3 / 9, 2.25 / 10]} px={'7px'} mb={'10px'}>
-        <MyCountryUpdate type={'deaths'}></MyCountryUpdate>
-      </Box>
-    </Flex>
+    <Fetch
+      service={() =>
+        CaseService.getUpdateCaseByCountry({ country: 'Thailand' })
+      }>
+      {({ data }) => {
+        return (
+          <Flex flexWrap="wrap" justifyContent="flex-end" mx={'-7px'}>
+            <Box width={[1 / 2, 1, 1, 3.5 / 10]} px={'7px'} mb={'15px'}>
+              <CardStatus data={data[0]} type={'confirmed'}></CardStatus>
+            </Box>
+            <Box
+              width={[1 / 2, 1 / 2, 4 / 8, 3.25 / 10]}
+              px={'7px'}
+              mb={'10px'}>
+              <CardStatus data={data[0]} type={'recovered'}></CardStatus>
+            </Box>
+            <Box
+              width={[1 / 2, 1 / 2, 4 / 8, 3.25 / 10]}
+              px={'7px'}
+              mb={'10px'}>
+              <CardStatus data={data[0]} type={'deaths'}></CardStatus>
+            </Box>
+          </Flex>
+        )
+      }}
+    </Fetch>
   )
 }
